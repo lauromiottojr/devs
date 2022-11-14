477cef2edb15fc40891f1c29a4c6c1e59bcf9f32
lauromiottojr <junior.miotto36@gmail.com>
2022-11-14 10:21:05 -0300

    Project created

---
+++ backend/src/test/java/com/devs/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devs.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
