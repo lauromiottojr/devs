37f78c48a6926fd2319278926dc3cae5d7ec8b7d
lauromiottojr <junior.miotto36@gmail.com>
2022-11-14 11:38:46 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from './assets/components/NotificationButton'
+
 function App() {
   return (
-    <h1>Hi world!</h1>
+    <>
+      <h1>Hi world!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
