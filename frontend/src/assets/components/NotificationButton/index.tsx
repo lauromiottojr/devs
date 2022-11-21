03ab3b351a618e0b7ab35d977ad89472d388f920
lauromiottojr <junior.miotto36@gmail.com>
2022-11-20 22:36:04 -0300

    Sending notification

--- frontend/src/assets/components/NotificationButton/index.tsx
+++ frontend/src/assets/components/NotificationButton/index.tsx
@@ -1,9 +1,22 @@
+import axios from 'axios';
+import { BASE_URL } from '../../../utils/request';
 import icon from '../../img/notification-icon.svg'
 import './styles.css'
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(saleId: number) {
+    // axios é uma biblioteca de requisição
+    axios(`${BASE_URL}/sales/${saleId}/notification`).then(response => {
+        console.log("Sucesso!");
+    })
+}
+
+function NotificationButton({ saleId }: Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
