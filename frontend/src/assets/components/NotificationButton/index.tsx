82f7578d0399ba38ae8c6aed0014e7e628061820
lauromiottojr <junior.miotto36@gmail.com>
2022-11-20 22:39:52 -0300

    Toast

--- frontend/src/assets/components/NotificationButton/index.tsx
+++ frontend/src/assets/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import { BASE_URL } from '../../../utils/request';
 import icon from '../../img/notification-icon.svg'
 import './styles.css'
@@ -10,7 +11,7 @@ type Props = {
 function handleClick(saleId: number) {
     // axios é uma biblioteca de requisição
     axios(`${BASE_URL}/sales/${saleId}/notification`).then(response => {
-        console.log("Sucesso!");
+        toast.info("SMS enviado com sucesso!");
     })
 }
 
